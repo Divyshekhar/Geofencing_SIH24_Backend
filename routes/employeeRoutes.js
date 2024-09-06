da3fd9ef552646c907e09a4111c41feb9995e607
Divyshekhar Sinha <divyshekharsinha@Divyshekhars-MacBook-Air.local>
2024-09-06 18:44:02 +0530

    adding signin

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const employeeController = require('../controllers/employeeController');
 
 router.post('/create', employeeController.createEmployeeWithPassword);
-router.post('/:id/verify-password', employeeController.verifyPassword);
+router.post('/:id/signin', employeeController.verifyPassword);
 router.get('/all', employeeController.allEmployee);
 router.get('/:id', employeeController.getEmployee);
 router.get('/:id/location', employeeController.getOfficeCoordinates);
